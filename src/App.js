ccb11b3f761a72fac5e06782b3e3b504bf43ca1f
Norbert <kamreal108@gmail.com>
2022-02-02 00:39:18 +0100

    new branch and starting layout is ready

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import './App.css';
+import React from "react";
+
+const API_URL = 'https://opentdb.com/api.php?amount=6'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='bg-blue-600 p-20 shadow-md rounded-lg '>
+        <div>Question x/6</div>
+        <div>Category</div>
+        <h2 className='text-3xl'>
+        This is the question box
+        </h2>
+      </div>
+      <div className="flex flex-wrap mt-4 justify-between content-between">
+        <div className='bg-blue-600 w-2/5 p-4 rounded-lg shadow-md font-semibold mb-4'>Answer 1</div>
+        <div className='bg-blue-600 w-2/5 p-4 rounded-lg shadow-md font-semibold mb-4'>Answer 2</div>
+        <div className='bg-blue-600 w-2/5 p-4 rounded-lg shadow-md font-semibold'>Answer 3</div>
+        <div className='bg-blue-600 w-2/5 p-4 rounded-lg shadow-md font-semibold'>Answer 4</div>
+      </div>
     </div>
   );
 }
