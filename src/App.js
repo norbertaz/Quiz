c5cce9bec138143f9232c59e492131b5d81c286f
Norbert <kamreal108@gmail.com>
2022-02-02 02:18:49 +0100

    random position for correct answer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from "react";
-
-const API_URL = 'https://opentdb.com/api.php?amount=6'
+import React, {useState, useEffect} from "react";
+import { Questionaire } from "./components";
+const API_URL = 'https://opentdb.com/api.php?amount=6&type=multiple'
 
 function App() {
-  return (
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then ((data) => {
+        setQuestions(data.results)
+      })
+  }, [])
+  const handleAnswer = (answer) => {
+    //bla bla
+  }
+  return questions.length > 0 ? (
     <div className="container">
-      <div className='bg-blue-600 p-20 shadow-md rounded-lg '>
-        <div>Question x/6</div>
-        <div>Category</div>
-        <h2 className='text-3xl'>
-        This is the question box
-        </h2>
-      </div>
-      <div className="flex flex-wrap mt-4 justify-between content-between">
-        <div className='bg-blue-600 w-2/5 p-4 rounded-lg shadow-md font-semibold mb-4'>Answer 1</div>
-        <div className='bg-blue-600 w-2/5 p-4 rounded-lg shadow-md font-semibold mb-4'>Answer 2</div>
-        <div className='bg-blue-600 w-2/5 p-4 rounded-lg shadow-md font-semibold'>Answer 3</div>
-        <div className='bg-blue-600 w-2/5 p-4 rounded-lg shadow-md font-semibold'>Answer 4</div>
-      </div>
+      <Questionaire data={questions[0]} handleAnswer={handleAnswer}/>
     </div>
-  );
+  ) : (<h2 className='text-2xl text-white font-bold'>Hey... We're loading questions</h2>);
 }
 
 export default App;
